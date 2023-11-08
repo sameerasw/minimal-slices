3c5296c1668da8556250b0af68ec2cdf6061de2f
Sameera Sandakelum <sameera.whoami@gmail.com>
2023-11-08 22:55:54 +0530

    Added an animated text as the title

--- js/script.js
+++ js/script.js
@@ -2,21 +2,62 @@ const slices = document.querySelectorAll(".slice");
 let focus = document.getElementById("focus");
 let stage = document.getElementById("stage");
 let preview = true;
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = null;
+let text = document.getElementById("splash-text");
 
 //display each slice with animation after the DOM loads
-window.onload = () => {
+function animateSlices() {
     slices.forEach((slice, i) => {
         setTimeout(() => {
             slice.classList.add("active");
             //add opacity 1 to each slice
             slice.style.opacity = 1;
-        }, (6-i) * 500);
+        }, (6-i) * 150);
     });
     setTimeout(() => {
         slices.forEach(s => s.classList.remove("active"));
         focus.classList.add("active");
         preview = false;
-    }, 3750);
+    }, 1500);
+}
+
+function textanimate (word) {
+    let iterations = 0;
+    const duration = 500 / word.length;
+
+    //if the new word is the same as the old one, do nothing
+    if (text.innerText === word) return;
+
+    //count and set the word length
+    text.innerText = word.toUpperCase();
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+        text.innerText = text.innerText
+        .split("")
+        .map((letter,index) => {
+            if(index < iterations){
+                return word[index];
+            }
+
+            let final = letters[Math.floor(Math.random() * 26)];
+
+            return final;
+
+        })
+        .join("");
+
+        if (iterations > word.length) {
+            clearInterval(interval);
+            text.style.opacity = 1.0;
+        } else {
+            text.style.opacity = 0.5;
+        }
+
+        iterations += 1;
+    } , duration);
 }
 
 
@@ -25,6 +66,7 @@ slices.forEach(slice => {
         if (preview) return;
         slices.forEach(s => s.classList.remove("active"));
         slice.classList.add("active");
+        textanimate(slice.dataset.value,0);
     });
     
     slice.addEventListener("mouseout", () => {
@@ -48,3 +90,10 @@ slices.forEach(slice => {
         }
     });
 });
+
+window.onload = () => {
+    textanimate("SAMEERA SANDAKELUM",7);
+
+    //start animation after 2 seconds
+    animateSlices();
+}
